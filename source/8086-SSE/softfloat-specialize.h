fdaa2117690c8d358b6b7ae46bcf795d6b5d9b95
srorso <stephen.orso@yahoo.com>
2016-10-14 10:46:56 -0400

    Corrected to build NaN propagation routines from hercsource

--- source/8086-SSE/softfloat-specialize.h
+++ source/8086-SSE/softfloat-specialize.h
@@ -37,8 +37,12 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef specialize_h
 #define specialize_h 1
 
-#include <stdbool.h>
-#include <stdint.h>
+#if !defined(false) 
+#include <stdbool.h> 
+#endif
+#if !defined(int32_t) 
+#include <stdint.h>             /* C99 standard integers */ 
+#endif
 #include "softfloat_types.h"
 
 /*----------------------------------------------------------------------------
