8e258f8b99ee20500bb52f0a80b19970dc2ff23b
Fish (David B. Trout) <fish@infidels.org>
2017-01-14 10:11:31 -0800

    Fix git revision count bug

--- cmake/modules/GIT_Revision.cmake
+++ cmake/modules/GIT_Revision.cmake
@@ -8,19 +8,53 @@
 #  Commit count
 #-----------------
 
-execute_process(
-    COMMAND ${GIT_EXECUTABLE} log --pretty=format:''
-    COMMAND wc -l
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    RESULT_VARIABLE _r
-    ERROR_VARIABLE  _e
-    ERROR_STRIP_TRAILING_WHITESPACE
-    OUTPUT_VARIABLE _o
-    OUTPUT_STRIP_TRAILING_WHITESPACE )
+if( WIN32 )
 
-if( NOT ${_r} EQUAL 0 )
-    message( FATAL_ERROR "Command \"git rev-list HEAD --count | wc -l\" failed with rc=${_r}: ${_e}" )
+    # PROGRAMMING NOTE: Windows does not come with "wc" so we have to
+    # use a different technique. Luckily most Windows versions of git
+    # are more modern than the gits that come with older Linux distros
+    # and thus very likely support the newer rev-list "--count" option.
+
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} rev-list --count HEAD
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        RESULT_VARIABLE _r
+        ERROR_VARIABLE  _e
+        ERROR_STRIP_TRAILING_WHITESPACE
+        OUTPUT_VARIABLE _o
+        OUTPUT_STRIP_TRAILING_WHITESPACE )
+
+    if( NOT ${_r} EQUAL 0 )
+        message( FATAL_ERROR "Command \"git rev-list --count HEAD\" failed with rc=${_r}: ${_e}" )
+    endif()
+
+else()
+
+    # PROGRAMMING NOTE: because some older Linux distros don't come with
+    # more recent versions of git that understand the rev-list "--count"
+    # option, we use the git 'log' command instead and pipe the results
+    # into wc to get the count. Also note that when we do this we need to
+    # be careful to use "tformat" and not "format" so that the last line
+    # gets properly terminated with a newline so that wc then returns the
+    # correct count. (Without "tformat" wc returns a count one less than
+    # the actual number of commits).
+
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} log --pretty=tformat:''
+        COMMAND wc -l
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        RESULT_VARIABLE _r
+        ERROR_VARIABLE  _e
+        ERROR_STRIP_TRAILING_WHITESPACE
+        OUTPUT_VARIABLE _o
+        OUTPUT_STRIP_TRAILING_WHITESPACE )
+
+    if( NOT ${_r} EQUAL 0 )
+        message( FATAL_ERROR "Command \"git log --pretty=tformat:'' | wc -l\" failed with rc=${_r}: ${_e}" )
+    endif()
+
 endif()
+
 
 string( STRIP ${_o} GIT_COMMIT_COUNT )
 trace( GIT_COMMIT_COUNT )
