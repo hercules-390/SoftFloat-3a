7055c221943d8ad47b01367d15f7a8441482b029
Stephen Orso <stephen.orso@yahoo.com>
2018-02-28 14:31:08 -0500

    Correctly identify Visual Studio 2008 as the build tool.  #8
    Change also defaults Visual Studio 2008 to Visual C++ 2008 Express edition and builds a 32-bit library.  Messages indicate this.

--- 1Stop.cmd
+++ 1Stop.cmd
@@ -16,21 +16,111 @@ set "retcode="
 pushd tools
 
 :: We check for PLATFORM because Windows is not very clear about whether
-:: we are building for amd64 or just running on amd64.  If PLATFORM
-:: exists, then this is a cross-platform build x86 on amd64.  Otherwise
-:: we will use the PROCESSOR_ARCHITECTURE value.  (Need to check
-:: what the variables look like for a cross-platform build amd64 on
-:: x86).   The result is available to the remaining scripts.
+:: we are building for amd64 or just running on amd64.  Visual Studio
+:: 2015 and later (and maybe earlier too) set PLATFORM to the architecture
+:: of the target.  Visual Studio 2008 does not set PLATFORM.
 
-if /I "%PLATFORM%"=="X86" (
-  set "build_arch=x86"
+:: If PLATFORM is set, then we can use its value without worry.
+
+:: if PLATFORM does not exist, then we must figure out the target
+:: architecture from PROCESSOR_ARCHITECTURE and, if present, from
+:: PROCESSOR_ARCHITEW6432.
+
+:: If PROCESSOR_ARCHITEW6432 does not exist, then PROCESSOR_ARCHITECTURE
+:: identifies the target architecture.  If PROCESSOR_ARCHITEW6432 does
+:: exist, then the build is taking place in a 32-bit command prompt on
+:: a 64-bit system, and PROCESSOR_ARCHITEW6432 describes the real
+:: hardware.
+
+:: Finally, if Visual Studio 2008 provides the compiler, we do not know
+:: if it is Express Edition, which builds 32-bit apps only, or Standard
+:: or better, which builds 32- or 64-bit apps.  Because 1Stop is the
+:: "easy to use, builds a working Hercules," we shall assume a 32-bit
+:: target, as that will work on a 64-bit system for many things one can
+:: do with Hercules.
+
+:: If a builder has Visual Studio 2008 Standard or better and needs a
+:: 64-bit build, 1Stop is not a solution that will work for them.
+
+if DEFINED PLATFORM (
+    set "build_arch=%PLATFORM%"
 ) else (
-  set "build_arch=%PROCESSOR_ARCHITECTURE%"
+    if DEFINED PROCESSOR_ARCHITEW6432 (
+        set "build_arch=%PROCESSOR_ARCHITEW6432%"
+    ) else (
+        set "build_arch=%PROCESSOR_ARCHITECTURE%"
+    )
 )
 
-:: Unforturnately the PLATFORM variable will mess up --clean-first.  We
-:: shall unset it for the duration of the script.  Setting the generator
-:: (below) will determine whether a 32-bit or 64-bit library is built.
+:: PLATFORM is either x86 or x64, PROCESSORS_ARCHITECTURE is either
+:: x86 or AMD64.  Conform x64 to AMD64
+
+If /I "%build_arch%"=="x64" (
+    set "build_arch=AMD64"
+)
+
+
+:: Set the CMake generator to the most modern version of Visual Studio
+:: installed on this machine.  This script assumes that at least one
+:: such is installed.  Lowest acceptable is VS2008; highest is VS2017.
+
+:: Visual Studio 2012 and higher command prompts can be identified by
+:: checking the environment variable VisualStudioVersion.  Older than
+:: 2012, we can parse the PATH variable, but that will fail if the
+:: builder installed Visual Studio in a non-default directory.  There
+:: is no help for that beyond understanding the difficulty and
+:: documenting it.
+
+:: If Visual Studio 9.0 2008 is identified as the the newest version
+:: installed, it is assumed to be the Express Edition.  VS2008EE is
+:: limited to building 32-bit applications out of the box, and 1Stop
+:: will therefore only build a 32-bit Hercules when VS2008EE is in use.
+:: (Yes, there are VS2008EE hacks to enable 64-bit builds, and a builder
+:: who uses those hacks will need to use the more flexible build tools
+:: like makefile.bat.)
+
+:: If there are multiple versions of Visual Studio installed, well,
+:: 1Stop  will end badly if someone wishes to use an older VS version to
+:: build.  In that case, we will expect the builder to use more flexible
+:: build tools.
+
+if "%VisualStudioVersion%" == "15.0" (
+    set "cmake_generator=Visual Studio 15 2017"
+) else if "%VisualStudioVersion%" == "14.0" (
+    set "cmake_generator=Visual Studio 14 2015"
+) else if "%VisualStudioVersion%" == "12.0" (
+    set "cmake_generator=Visual Studio 12 2013"
+) else if "%VisualStudioVersion%" == "11.0" (
+    set "cmake_generator=Visual Studio 11 2012"
+) else if "%VisualStudioVersion%" == "10.0" (
+    set "cmake_generator=Visual Studio 10 2010"
+) else if "%VisualStudioVersion%" == "9.0" (
+    set "cmake_generator=Visual Studio 9 2008"
+) else if NOT "%PATH:Visual Studio 10.0=%"=="%PATH%" (
+    set "cmake_generator=Visual Studio 10 2010"
+) else if NOT "%PATH:Visual Studio 9.0=%"=="%PATH%" (
+    echo Visual Studio 9 ^(2008^) identified as build tool.  Assuming Express Edition.
+    echo Building 32-bit library.
+    set "build_arch=x86"
+    set "cmake_generator=Visual Studio 9 2008"
+) else (
+    set "retcode=12"
+    echo Unable to determine Visual Studio version.
+    echo Unable to match VisualStudioVersion "%VisualStudioVersion%" to a valid version.
+    echo Unable to to determine version from PATH environment variable.
+    echo Terminating.
+    exit /b 0
+)
+
+:: If building for 64-bit target, set the suffix for the CMake generator
+if /I "%build_arch%" == "AMD64" (
+    set "cmake_generator=%cmake_generator% Win64"
+)
+
+:: Unfortunately the PLATFORM variable will mess up --clean-first.  We
+:: shall unset it for the duration of the script.  The cmake_generator
+:: variable set above will determine whether a 32-bit or 64-bit library
+:: is built.
 
 set "PLATFORM="
 
--- tools/BuildMe.cmd
+++ tools/BuildMe.cmd
@@ -9,42 +9,15 @@
 :: %build_arch% must have been set to the build architecture by the caller
 :: of this script
 
+:: %cmake_generator% must have been set to the CMake generator to be used,
+:: as determined by the caller based on the Visual Studio version.
+
 echo ************************************** %0: Configuring and Building SoftFloat 3a
 
 set "todir=..\..\%build_arch%\s3fh.release"
 echo Building for release in '%todir%'. >&2
 if "%1" neq "" (
     echo ..with parameters %*
-)
-
-:: Set the CMake generator to the most modern version of Visual Studio
-:: installed on this machine.  This script assumes that at least one
-:: such is installed.  Lowest acceptable is VS2008; highest is VS2017
-:: (As of this writing, VS2017 is supported by CMake but is only RC
-:: status from Microsoft.
-
-if "%VisualStudioVersion%" == "15.0" (
-    set "cmake_generator=Visual Studio 15 2017"
-) else if "%VisualStudioVersion%" == "14.0" (
-    set "cmake_generator=Visual Studio 14 2015"
-) else if "%VisualStudioVersion%" == "12.0" (
-    set "cmake_generator=Visual Studio 12 2013"
-) else if "%VisualStudioVersion%" == "11.0" (
-    set "cmake_generator=Visual Studio 11 2012"
-) else if "%VisualStudioVersion%" == "10.0" (
-    set "cmake_generator=Visual Studio 10 2010"
-) else if "%VisualStudioVersion%" == "9.0" (
-    set "cmake_generator=Visual Studio 9 2008"
-) else (
-    set "retcode=12"
-    echo Unable to determine Visual Studio version, found "%VisualStudioVersion%".  Terminating.
-    goto :exitBuildMe
-)
-
-:: If building for 64-bit target, set the suffix for the CMake generator
-
-if /I "%build_arch%" == "amd64" (
-    set "cmake_generator=%cmake_generator% Win64"
 )
 
 
@@ -56,6 +29,7 @@ if %errorlevel% GTR 0 (
     goto :exitBuildMe
 )
 
+echo cmake ..\..\SoftFloat-3a -G "%cmake_generator%%"
 
 cmake ..\..\SoftFloat-3a -G "%cmake_generator%%"
 
